0811c80359c9c6ca8dc32db20d5b6d37667204f1
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-02-26 10:03:01 +0800

    add gem rack-cors

--- Gemfile
+++ Gemfile
@@ -23,7 +23,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem 'groupdate'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
-# gem 'rack-cors'
+ gem 'rack-cors'
 
 
 group :production do
--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative "boot"
+ require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -36,5 +36,13 @@ module Backendiot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put, :options]
+      end
+    end
+
   end
 end
