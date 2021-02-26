0ebfe32e7af66ec0cdb397d8a185d5df70e7b615
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-02-26 10:51:03 +0800

    fix api ammonia

--- app/controllers/api/v1/display_controller.rb
+++ app/controllers/api/v1/display_controller.rb
@@ -1,7 +1,7 @@
 module Api
     module V1
         class DisplayController<ApplicationController
-            # display data counter if not understand this query... can try using get api to set the result 
+            # display data counter  by ID Device and latest date if not understand this query... can try using get api to see the result 
             def index
                 detail=Detailcounter.find_by_sql(" SELECT detailcounters.id,detailcounters.counter_id,detailcounters.updated_at,counters.location,detailcounters.count from ((detailcounters 
                 inner join (select detailcounters.counter_id, max(detailcounters.updated_at)as MaxDate from detailcounters group by detailcounters.counter_id)tm on detailcounters.counter_id=tm.counter_id and detailcounters.updated_at=tm.MaxDate)inner join counters on detailcounters.counter_id=counters.id) GROUP BY DATE (detailcounters.updated_at),detailcounters.id,counters.location")
