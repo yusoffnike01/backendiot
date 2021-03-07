4791762e285b47a02845afcf73eed24b83c25e36
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-03-07 10:42:50 +0800

    update cors

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Backendiot
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :delete, :patch, :put, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
       end
     end
 
